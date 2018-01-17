2f19c57030e88a6507d7436a1648cbcf98057a23
alpha_meng <mbqaerfa@qq.com>
2018-01-17 19:12:42 +0800

    结构提交

---
+++ sutdio/newbie/conf/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os, sys, time, re, random, csv, json
+
+"""
+@version: 0.0.1
+@author: Jason_Meng
+@contact: mbqaerfa@qq.com
+@software: PyCharm
+@project: 101Studio
+@file: __init__.py.py
+@time: 2018/1/17 19:09
+"""
+
+if __name__ == "__main__":
+    pass
